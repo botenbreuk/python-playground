7c874b2d5d09614e399d1cfda1b552ab958642de
Robin <habo-robin@live.nl>
2023-10-15 22:07:44 +0200

    Added basic types to the utils.py and added comments to main.py

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 from utils import read_grid
 import logging
 
+# Logger config
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s',
                     datefmt='%d-%m-%Y %H:%M:%S', level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+# Print and basic variables
 print("My first program")
 price = 100
 qty = 5
 total = price * qty
 print("Total = ", total)
 
-for i in range(11):
+# Simple forloop
+for i in range(10):
     print(i)
 
-
+# Dictionary and logger
 textObject = {'key': 'value'}
 print(textObject['key'])
 log.info('Hello world! Var: {}'.format("Test"))
 
+# dictionary list and retrieve data
 testObject2 = [{'name': 'Jaap'}, {'name': 'Piet'}]
 for i in range(len(testObject2)):
     print(testObject2[0]["name"])
 
+# String splitting and for loop with filter
 newList = []
 test = "0 || 0 || 0 || 0 || 0".split()
 for x in test:
     if x == "||":
         continue
     newList.append(x)
-
 print(newList)
 
-# testInput = str(input("Positie? \n"))
-# print([*testInput])
-
+# Read grid txt files and create 2D array
 with open('grid.txt') as f:
     grid = read_grid(f.readlines())
-
 print(grid)
 print(grid[4][2])
+
+# Command lint input and output and character splitting
+testInput = str(input("Positie? \n"))
+print([*testInput])
--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
-def read_grid(lines: list[str]):
-    grid = []
+def read_grid(lines: list[str]) -> list[list[str]]:
+    grid: list[list[str]] = []
     for line in lines:
-        value = to_line_array(line)
+        value = __to_line_array(line)
         grid.append(value)
     return grid
 
 
-def to_line_array(line: str):
-    arr = []
+def __to_line_array(line: str) -> list[str]:
+    arr: list[str] = []
     splitted = line.strip().split()
     for i in splitted:
         if i == '||':
